57c208f3450cf59d81dd4d9a5e4f90b9309da5c4
GNooNJo <jjjchoon@nate.com>
2024-01-07 18:39:06 +0900

    Add Annotation

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// TMDB API Authentication Token
 const options = {
   method: "GET",
   headers: {
@@ -9,13 +10,17 @@ const options = {
 
 const url = "https://api.themoviedb.org/3/movie/top_rated?language=ko-KR";
 
+// window.onload
 window.onload = function () {
+  // title.onclick => reload
   const cineDocsTitle = document.getElementById("cineDocsTitle");
   cineDocsTitle.addEventListener("click", function (e) {
     window.location.reload();
   });
 
+  // 검색창
   const inputText = document.getElementById("search_text");
+  // If user press "Enter"
   inputText.addEventListener("keydown", function (e) {
     if (e.key === "Enter") {
       e.preventDefault();
@@ -23,15 +28,18 @@ window.onload = function () {
     }
   });
 
+  // [검색] 버튼
   const searchButton = document.getElementById("search_btn");
   searchButton.addEventListener("click", showSearchResults);
 
+  // TMDB API data 가져오기
   fetch(url, options)
     .then((response) => response.json())
     .then((data) => {
       const movie_list = document.querySelector(".movie_list");
       const movie_data = data["results"];
 
+      // API의 정보를 HTML에 담아, return HTML Array
       const movie_result = movie_data.map((movie) => {
         let movie_html = `
       <div class="movie_card" onclick="alert('[${movie["title"]}] 의 Movie ID : ${movie["id"]}')">
@@ -47,24 +55,30 @@ window.onload = function () {
         return movie_html;
       });
 
+      // return된 HTML Array를 화면에 출력
       movie_result.forEach((movie) => {
         movie_list.innerHTML += movie;
       });
     })
     .catch((err) => console.error(err));
 
+  // 검색 결과 출력
   function showSearchResults() {
+    // TMDB API 가져오기
     fetch(url, options)
       .then((res) => res.json())
       .then((data) => {
         const movie_list = document.querySelector(".movie_list");
         const movie_data = data["results"];
+        // 검색 결과 Array return
         const searched_list = movie_data.filter((movie) => {
           return (
             movie["title"].includes(inputText.value) ||
             movie["original_title"].toUpperCase().includes(inputText.value.toUpperCase())
           );
         });
+
+        // 검색 결과 Array에 따라 return HTML Array
         const searched_result = searched_list.map((movie) => {
           let movie_html = `
       <div class="movie_card" onclick="alert('[${movie["title"]}] 의 Movie ID : ${movie["id"]}')">
@@ -80,7 +94,10 @@ window.onload = function () {
           return movie_html;
         });
 
+        // 기존 HTML 초기화
         movie_list.innerHTML = "";
+
+        // return된 HTML Array를 화면에 출력
         searched_result.forEach((movie) => {
           movie_list.innerHTML += movie;
         });
