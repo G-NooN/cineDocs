fda6b2a34b204045e1fe002256fe00816775fecd
GNooNJo <jjjchoon@nate.com>
2024-01-09 03:34:08 +0900

    Refactor: Add export & import, add menu eventListener

--- src/main.js
+++ src/main.js
@@ -1,106 +1,32 @@
-// TMDB API Authentication Token
-const options = {
-  method: "GET",
-  headers: {
-    accept: "application/json",
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhYjBmMTZkZjFlYjA4YmZjOGQxNWQ3OGJjYjBlMGM4NyIsInN1YiI6IjY1OTkyYTQ0ODliNTYxMDIwMTJkMTIwYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.AopUxBQtjeqovOo23P_ix9YfZafyz2YYi8G-AkDAKWk",
-  },
-};
+import { generateMovieList } from "./showMovies.js";
+import { showSearchResult } from "./search.js";
 
-const url = "https://api.themoviedb.org/3/movie/top_rated?language=ko-KR";
+const $popular = document.querySelector("#popular");
+const $topRated = document.querySelector("#topRated");
 
-// window.onload
-window.onload = function () {
-  // title.onclick => reload
-  const cineDocsTitle = document.getElementById("cineDocsTitle");
-  cineDocsTitle.addEventListener("click", function (e) {
-    window.location.reload();
-  });
+generateMovieList("top_rated");
 
-  // 검색창
-  const inputText = document.getElementById("search_text");
-  // If user press "Enter"
-  inputText.addEventListener("keydown", function (e) {
-    if (e.key === "Enter") {
-      e.preventDefault();
-      showSearchResults();
-    }
-  });
+const searchInput = document.querySelector("#search-text");
 
-  // [검색] 버튼
-  const searchButton = document.getElementById("search_btn");
-  searchButton.addEventListener("click", showSearchResults);
+const form = document.querySelector("#search-form");
 
-  const movie_list = document.querySelector(".movie_list");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  showSearchResult(searchInput.value);
+});
 
-  // TMDB API data 가져오기
-  fetch(url, options)
-    .then((response) => response.json())
-    .then((data) => {
-      const movie_data = data["results"];
-      // API의 정보를 HTML에 담아, return HTML Array
-      const movie_result = movie_data.map((movie) => {
-        let movie_html = `
-      <div class="movie_card" onclick="alert('[${movie["title"]}] 의 Movie ID : ${movie["id"]}')">
-        <div class="movie_content">
-          <h2>${movie["title"]} (${movie["original_title"]})</h2>
-          <p><strong>개봉일</strong> : ${movie["release_date"]} / <strong>평점</strong> : ${movie["vote_average"]}</p>
-          <p>${movie["overview"]}</p>
-        </div>
-        <div class="movie_img">
-          <img src='https://image.tmdb.org/t/p/w185${movie["poster_path"]}' />
-        </div>
-      </div>`;
-        return movie_html;
-      });
+const $h1 = document.querySelector("h1");
+$h1.addEventListener("click", (event) => {
+  event.preventDefault();
+  window.location.reload();
+});
 
-      // return된 HTML Array를 화면에 출력
-      movie_result.forEach((movie) => {
-        movie_list.innerHTML += movie;
-      });
-    })
-    .catch((err) => console.error(err));
+$popular.addEventListener("click", (event) => {
+  event.preventDefault();
+  generateMovieList("popular");
+});
 
-  // 검색 결과 출력
-  function showSearchResults() {
-    // TMDB API 가져오기
-    fetch(url, options)
-      .then((res) => res.json())
-      .then((data) => {
-        const movie_data = data["results"];
-        // 검색 결과 Array return
-        const searched_list = movie_data.filter((movie) => {
-          return (
-            movie["title"].includes(inputText.value) ||
-            movie["original_title"].toUpperCase().includes(inputText.value.toUpperCase())
-          );
-        });
-
-        // 검색 결과 Array에 따라 return HTML Array
-        const searched_result = searched_list.map((movie) => {
-          let movie_html = `
-      <div class="movie_card" onclick="alert('[${movie["title"]}] 의 Movie ID : ${movie["id"]}')">
-        <div class="movie_content">
-          <h2>${movie["title"]} (${movie["original_title"]})</h2>
-          <p><strong>개봉일</strong> : ${movie["release_date"]} / <strong>평점</strong> : ${movie["vote_average"]}</p>
-          <p>${movie["overview"]}</p>
-        </div>
-        <div class="movie_img">
-          <img src='https://image.tmdb.org/t/p/w185${movie["poster_path"]}' />
-        </div>
-      </div>`;
-          return movie_html;
-        });
-
-        // 기존 HTML 초기화
-        movie_list.innerHTML = "";
-
-        // return된 HTML Array를 화면에 출력
-        searched_result.forEach((movie) => {
-          movie_list.innerHTML += movie;
-        });
-      })
-      .catch((err) => console.log(err));
-  }
-};
+$topRated.addEventListener("click", (event) => {
+  event.preventDefault();
+  generateMovieList("top_rated");
+});
