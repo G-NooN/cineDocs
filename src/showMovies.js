06bffe89a3126a45b5f9a86371754fa18ff6b506
GNooNJo <jjjchoon@nate.com>
2024-01-11 02:19:55 +0900

    Refactor: change tag from <li> to <div>

--- src/showMovies.js
+++ src/showMovies.js
@@ -6,12 +6,12 @@ export const generateMovieList = async (urlInfo) => {
   $movieList.innerHTML = movies
     .map(
       (movie) => `
-  <li class="movie-item" id="${movie.id}">
+  <div class="movie-item" id="${movie.id}">
     <img src="https://image.tmdb.org/t/p/w185${movie.poster_path}" alt="${movie.title}">
     <h2><span class="movie-title">${movie.title}</span> (<span class="movie-original-title">${movie.original_title}</span>)</h2>
     <p><strong>개봉일</strong> : ${movie.release_date} / <strong>평점</strong> : ${movie.vote_average}</p>
     <p>${movie.overview}</p>
-  </li>`
+  </div>`
     )
     .join("");
 
