bc033d3b8056f518fe603c0527c919961a165d60
GNooNJo <jjjchoon@nate.com>
2024-01-14 16:19:58 +0900

    Fix: 기존 if문 -> 삼항 연산자(?:) 적용

--- src/search.js
+++ src/search.js
@@ -7,14 +7,10 @@ export const showSearchResult = (searchKeyword) => {
     const title = item.querySelector(".movie-title").textContent; // 한국어 제목
     const originalTitle = item.querySelector(".movie-original-title").textContent.toUpperCase(); // 원제
 
-    if (
-      title.includes(searchedValue) ||
-      title.toUpperCase().includes(searchedValue.toUpperCase()) ||
-      originalTitle.includes(searchedValue.toUpperCase())
-    ) {
-      item.style.display = "block";
-    } else {
-      item.style.display = "none";
-    }
+    title.includes(searchedValue) ||
+    title.toUpperCase().includes(searchedValue.toUpperCase()) ||
+    originalTitle.includes(searchedValue.toUpperCase())
+      ? (item.style.display = "block")
+      : (item.style.display = "none");
   });
 };
