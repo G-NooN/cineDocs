1567988edb957a927bdd24cb8386cca2b1f0c71b
GNooNJo <jjjchoon@nate.com>
2024-01-14 14:10:16 +0900

    Fix: 검색값 변수 위치 변경

--- src/search.js
+++ src/search.js
@@ -1,11 +1,11 @@
 // 검색 결과 출력
 export const showSearchResult = (searchKeyword) => {
   const movieItems = document.querySelectorAll(".movie-item");
+  const searchedValue = searchKeyword;
 
   movieItems.forEach((item) => {
     const title = item.querySelector(".movie-title").textContent; // 한국어 제목
     const originalTitle = item.querySelector(".movie-original-title").textContent.toUpperCase(); // 원제
-    const searchedValue = searchKeyword;
 
     if (
       title.includes(searchedValue) ||
