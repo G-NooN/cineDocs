703b70ce11cb9c421c2cc673c299d8048890d964
GNooNJo <jjjchoon@nate.com>
2024-01-16 01:19:36 +0900

    Add: 검색 결과 없을 시 alert 출력

--- src/search.js
+++ src/search.js
@@ -13,4 +13,17 @@ export const showSearchResult = (searchKeyword) => {
       ? (item.style.display = "block")
       : (item.style.display = "none");
   });
+  if (areAllElementsHidden(movieItems)) {
+    alert("검색 결과가 존재하지 않습니다.");
+    return;
+  }
 };
+
+function areAllElementsHidden(nodeList) {
+  for (let i = 0; i < nodeList.length; i++) {
+    if (window.getComputedStyle(nodeList[i]).display !== "none") {
+      return false;
+    }
+  }
+  return true;
+}
